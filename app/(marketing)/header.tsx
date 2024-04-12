b69a8940b9f6d02cff4c0b3c4c05ea9403af0cd2
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2024-04-12 14:08:06 +0300

    Update styles and add animations to globals.css Remove unused page.tsx file

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import useUserStore from "@/stores/user-store";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  UserButton,
+  useSignIn,
+  useUser,
+} from "@clerk/nextjs";
+import { ArrowUpRight, Loader } from "lucide-react";
+import React from "react";
+
+const MainHeader = () => {
+  const { isSignedIn } = useUser();
+
+  return (
+    <header className="h-20 hidden  w-full px-4 md:block fixed">
+      <div className="mx-auto flex items-center justify-between h-full ">
+        <div className=" pl-4 flex items-center gap-x-3">
+          {/* <Image src="/mascot.svg" height={40} width={40} alt="Mascot" /> */}
+          <div className="size-8 rounded-full bg-neutral-100 font-bold text-xl flex items-center justify-center text-blue-500"></div>
+          <h1 className="text-lg font-extrabold  tracking-wide">
+            Events Manager
+          </h1>
+        </div>
+
+        <div className="p-4 flex flex-row gap-x-2">
+          <Button
+            className={`text-white hover:text-blue-500 flex flex-row ${
+              !isSignedIn ? "bg-blue-500 " : "bg-green-500"
+            } h-8 rounded-full`}
+            variant="ghost"
+          >
+            {!isSignedIn ? "Join early" : "Joined"}
+            <ArrowUpRight className="w-4 h-4 ml-2" />
+          </Button>
+
+          <ClerkLoading>
+            <Loader className="w-5 h-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+
+          <ClerkLoaded>
+            <UserButton afterSignOutUrl="/" />
+          </ClerkLoaded>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default MainHeader;
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import MainHeader from "./header";
+import { HiPaperAirplane } from "react-icons/hi2";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import {
+  ClerkLoaded,
+  SignInButton,
+  SignUpButton,
+  SignedOut,
+  useSignIn,
+  useUser,
+} from "@clerk/nextjs";
+
+export default function Home() {
+  const words = ["brochures", "flyers", "guides", "leaflets"];
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  const { isSignedIn } = useUser();
+
+  const { signIn, isLoaded } = useSignIn();
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 2000); // Change the word every 2 seconds
+
+    return () => clearInterval(intervalId); // Clean up the interval on component unmount
+  }, []);
+
+  return (
+    <main className="flex-1 w-full flex-col h-screen relative bg-gradient-to-r from-rose-50 to-teal-50">
+      <MainHeader />
+      <div className="md:max-w-[1080px] mx-auto flex flex-1 w-full flex-col p-4 md:flex-row h-full gap-y-6 gap-2">
+        <div className="flex flex-1  flex-col justify-center gap-y-10">
+          <h1 className="text-5xl font-extrabold">
+            <strong>
+              No more{" "}
+              <span className={`text-red-500 stroke-red-500 text-animation`}>
+                {words[currentWordIndex]}
+              </span>{" "}
+              to follow what&apos;s happening at events...
+            </strong>
+          </h1>
+
+          <div className="flex flex-col">
+            <div className="flex flex-row items-center gap-x-1">
+              <HiPaperAirplane />
+              <h1>Use a timer to track the flow of your event.</h1>
+            </div>
+            <div className="flex flex-row items-center gap-x-1">
+              <HiPaperAirplane />
+              <h1>Send live Polls to engage with the audience.</h1>
+            </div>
+            <div className="flex flex-row items-center gap-x-1">
+              <HiPaperAirplane />
+              <h1>Share the rundown with your audience to flow through.</h1>
+            </div>
+            <div className="flex flex-row items-center gap-x-1">
+              <HiPaperAirplane />
+              <h1>Pause, Rewind and Skip event activities in Real time.</h1>
+            </div>
+          </div>
+
+          <div className="flex flex-row justify-between items-center">
+            {isSignedIn ? (
+              <h1 className="text-sm wiggle-animation">
+                🎉 Thanks for your interest in EventManager. 🤩
+              </h1>
+            ) : (
+              <ClerkLoaded>
+                <SignedOut>
+                  <SignUpButton
+                    mode="modal"
+                    afterSignInUrl="/"
+                    afterSignUpUrl="/"
+                  >
+                    <Button
+                      className="rounded-md text-lg tracking-wide  text-white h-12"
+                      size="lg"
+                    >
+                      Create a program
+                    </Button>
+                  </SignUpButton>
+                </SignedOut>
+              </ClerkLoaded>
+            )}
+
+            <div className="flex flex-row items-center gap-x-2">
+              <p className="text-xs mb-2">See it in action </p>
+              <h1 className="md:hidden">👇👇</h1>
+              <Image
+                src="/doodle.svg"
+                height={40}
+                width={100}
+                alt="Doodle"
+                className="hidden md:block"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* Right */}
+        <div className="flex flex-1 items-center justify-center md:pl-5">
+          <div className="w-full h-full md:h-1/2 bg-neutral-300 rounded-md"></div>
+        </div>
+      </div>
+    </main>
+  );
+}
